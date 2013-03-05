2ffa9ec0cf0006df8e4f533abaa844867f3ffbc4
Sergey Malykh <xronos.i.am@gmail.com>
2013-03-05 09:14:26 +0400

    fixed redirects when request.uri is not equal to request.env['REQUEST_URI']

--- lib/wiselinks/controller_methods.rb
+++ lib/wiselinks/controller_methods.rb
@@ -21,7 +21,9 @@ module Wiselinks
     end
 
     def set_wiselinks_url      
-      self.response.headers['X-Wiselinks-Url'] = request.url if self.request.wiselinks?
+      if self.request.wiselinks?
+        self.response.headers['X-Wiselinks-Url'] = request.base_url + request.env['REQUEST_URI'].gsub( /\&_=\d+/, '' )
+      end
     end
   end
 end
