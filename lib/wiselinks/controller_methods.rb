5a06b520834a6ee5706095723547480fbba8cb68
Sergey Malykh <xronos.i.am@gmail.com>
2013-03-05 12:00:06 +0400

    ?_=

--- lib/wiselinks/controller_methods.rb
+++ lib/wiselinks/controller_methods.rb
@@ -22,7 +22,7 @@ module Wiselinks
 
     def set_wiselinks_url      
       if self.request.wiselinks?
-        self.response.headers['X-Wiselinks-Url'] = request.base_url + request.env['REQUEST_URI'].gsub( /\&_=\d+/, '' )
+        self.response.headers['X-Wiselinks-Url'] = request.base_url + request.env['REQUEST_URI'].gsub( /[\&\?]_=\d+/, '' )
       end
     end
   end
